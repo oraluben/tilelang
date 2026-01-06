238058373272bcc66046d9478f0c989eb2398f18
WuGuiming <guiming.wu@mthreads.com>
2026-01-06 20:01:49 +0800

    Revert "Merge branch 'feat/yunqiao.jiang/gemm_l2_blk_swizzle' into 'main'"
    This reverts merge request !1

--- src/tl_templates/musa/threadblock_swizzle.h
+++ src/tl_templates/musa/threadblock_swizzle.h
@@ -6,57 +6,37 @@ namespace tl {
 
 template <int panel_width> TL_DEVICE dim3 rasterization2DRow() {
   const unsigned int block_idx = blockIdx.x + blockIdx.y * gridDim.x;
+  const unsigned int grid_size = gridDim.x * gridDim.y;
   const unsigned int panel_size = panel_width * gridDim.x;
-  const unsigned int res_panel_width = gridDim.y % panel_width;
-  const unsigned int total_panel = gridDim.y / panel_width * panel_width * gridDim.x;
-  const unsigned int panel_idx = block_idx / panel_size;
   const unsigned int panel_offset = block_idx % panel_size;
-
-  unsigned int mini_x;
-  unsigned int row_idx, col_idx;
-  if (block_idx >= total_panel) {
-    col_idx = panel_offset / res_panel_width;
-    mini_x = panel_offset % res_panel_width;
-    mini_x = (col_idx & 1) ? res_panel_width - 1 - mini_x : mini_x;
-  } else {
-    col_idx = panel_offset / panel_width;
-    mini_x = panel_offset % panel_width;
-    mini_x = (col_idx & 1) ? panel_width - 1 - mini_x : mini_x;
-  }
-
-  if (panel_idx & 1) {
-    col_idx = gridDim.x - 1 - col_idx;
-  }
-  row_idx = panel_idx * panel_width + mini_x;
-
+  const unsigned int panel_idx = block_idx / panel_size;
+  const unsigned int total_panel = mutlass::ceil_div(grid_size, panel_size);
+  const unsigned int stride =
+      panel_idx + 1 < total_panel
+          ? panel_width
+          : (grid_size - panel_idx * panel_size) / gridDim.x;
+  const unsigned int col_idx = (panel_idx & 1)
+                                   ? gridDim.x - 1 - panel_offset / stride
+                                   : panel_offset / stride;
+  const unsigned int row_idx = panel_offset % stride + panel_idx * panel_width;
   return {col_idx, row_idx, blockIdx.z};
 }
 
 template <int panel_width> TL_DEVICE dim3 rasterization2DColumn() {
   const unsigned int block_idx = blockIdx.x + blockIdx.y * gridDim.x;
+  const unsigned int grid_size = gridDim.x * gridDim.y;
   const unsigned int panel_size = panel_width * gridDim.y;
-  const unsigned int res_panel_width = gridDim.x % panel_width;
-  const unsigned int total_panel = gridDim.x / panel_width * panel_width * gridDim.y;
-  const unsigned int panel_idx = block_idx / panel_size;
   const unsigned int panel_offset = block_idx % panel_size;
-
-  unsigned int mini_x;
-  unsigned int row_idx, col_idx;
-  if (block_idx >= total_panel) {
-    row_idx = panel_offset / res_panel_width;
-    mini_x = panel_offset % res_panel_width;
-    mini_x = (row_idx & 1) ? res_panel_width - 1 - mini_x : mini_x;
-  } else {
-    row_idx = panel_offset / panel_width;
-    mini_x = panel_offset % panel_width;
-    mini_x = (row_idx & 1) ? panel_width - 1 - mini_x : mini_x;
-  }
-
-  if (panel_idx & 1) {
-    row_idx = gridDim.y - 1 - row_idx;
-  }
-  col_idx = panel_idx * panel_width + mini_x;
-
+  const unsigned int panel_idx = block_idx / panel_size;
+  const unsigned int total_panel = mutlass::ceil_div(grid_size, panel_size);
+  const unsigned int stride =
+      panel_idx + 1 < total_panel
+          ? panel_width
+          : (grid_size - panel_idx * panel_size) / gridDim.y;
+  const unsigned int row_idx = (panel_idx & 1)
+                                   ? gridDim.y - 1 - panel_offset / stride
+                                   : panel_offset / stride;
+  const unsigned int col_idx = panel_offset % stride + panel_idx * panel_width;
   return {col_idx, row_idx, blockIdx.z};
 }
 
